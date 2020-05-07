10d222b4922526f08fa964654c9ee46b48b23587
Chirila <george.chirila@centric.eu>
2020-05-07 17:34:02 +0300

    Second commit

--- AutomationSolution/AutomationSolution/LoginTests.cs
+++ AutomationSolution/AutomationSolution/LoginTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using AutomationSolution.PageObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,21 +10,56 @@ namespace AutomationSolution
     [TestClass]
     public class LoginTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private LoginPage loginPage;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
-            var driver = new ChromeDriver();
+            driver = new ChromeDriver();
+            loginPage = new LoginPage(driver);
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
             driver.FindElement(By.Id("sign-in")).Click();
             Thread.Sleep(1000);
-            driver.FindElement(By.CssSelector("input[data-test='email']")).SendKeys("asd@asd.asd");
-            driver.FindElement(By.CssSelector("input[type='password']")).SendKeys("asd");
-            driver.FindElement(By.XPath("//input[@value='Sign in']")).Click();
+        }
 
-            Assert.AreEqual("asd@asd.asd",
-                driver.FindElement(By.CssSelector("span[data-test='current-user']")).Text);
+        [TestMethod]
+        public void Login_CorrectEmail_CorrectPassword()
+        {
+            loginPage.LoginApplication("asd@asd.asd", "asd");
 
+            var expectedResult = "asd@asd.asd";
+            var actualResult = driver.FindElement(By.CssSelector("span[data-test='current-user']")).Text;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Login_IncorrectEmail_CorrectPassword()
+        {
+            loginPage.LoginApplication("asd12321414@asd12321.asd312", "asd");
+
+            var expectedResult = "Bad email or password.";
+            var actualResults = driver.FindElement(By.XPath("//div[contains(@class,'alert')]")).Text;
+
+            Assert.AreEqual(expectedResult, actualResults);
+        }
+
+        [TestMethod]
+        public void Login_CorrectEmail_IncorrectPassword()
+        {
+            loginPage.LoginApplication("asd@asd.asd", "asd1234");
+
+            var expectedResult = "Bad email or password.";
+            var actualResults = driver.FindElement(By.XPath("//div[contains(@class,'alert')]")).Text;
+
+            Assert.AreEqual(expectedResult, actualResults);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
             driver.Quit();
         }
     }
---
+++ AutomationSolution/AutomationSolution/PageObjects/LoginPage.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+
+namespace AutomationSolution.PageObjects
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver browser)
+        {
+            driver = browser;
+        }
+
+        private IWebElement TxtEmail()
+        {
+            return driver.FindElement(By.CssSelector("input[data-test='email']"));
+        }
+
+        private IWebElement TxtPassword()
+        {
+            return driver.FindElement(By.CssSelector("input[type='password']"));
+        }
+
+        private IWebElement BtnLogin()
+        {
+            return driver.FindElement(By.XPath("//input[@value='Sign in']"));
+        }
+
+        public void LoginApplication(string email, string password)
+        {
+            TxtEmail().SendKeys(email);
+            TxtPassword().SendKeys(password);
+            BtnLogin().Click();
+        }
+    }
+}
